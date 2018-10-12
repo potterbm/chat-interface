275b148f6128cfda14eeef29986642d0376ca7b9
Bryan Potter <potterbm@gmail.com>
2018-10-11 20:27:51 -0600

    timeframe upgrade

--- src/components/processInput/detectTimeframe.js
+++ src/components/processInput/detectTimeframe.js
@@ -1,3 +1,4 @@
+import moment          from 'moment';
 import stemToNumber    from '../../lib/stemToNumber';
 import unitOfTimeStems from '../../constants/unitOfTimeStems';
 
@@ -9,43 +10,84 @@ function findUnitOfTime(stems) {
   return -1;
 }
 
-function getDurationFromStems(stems, timeframe) {
-  timeframe.duration = 0;
-  timeframe.durationStems = [];
+function getDurationFromStems(timeframe) {
+  let duration = 0;
+  let n = timeframe.raw.unitOfTimeIndex - 1;
+
+  for (n; n >= 0; n--) {
+    if (stemToNumber(timeframe.raw.stems[n]) === undefined) break;
+
+    duration += stemToNumber(timeframe.raw.stems[n]);
+    timeframe.raw.durationStems.push(timeframe.raw.stems[n]);
+  }
+
+  timeframe.raw.durationStartIndex = n + 1;
 
   // Time ago
-  if (stems[timeframe.unitOfTimeIndex + 1] === 'go') {
-    // keep duration inferred as 1 day, transition the duration code to change the start time
+  // Keep duration inferred as 1, duration number changes the start time
+  if (timeframe.raw.stems[timeframe.raw.unitOfTimeIndex + 1] === 'go') {
+    timeframe.raw.durationStartIndex = n;
+    timeframe.raw.durationStems.push(timeframe.raw.stems[n]);
+
+    timeframe.duration = 1;
+    timeframe.start = moment.utc()
+      .startOf(timeframe.unitOfTime)
+      .subtract(duration, timeframe.unitOfTime)
+      .toISOString();
+    return;
   }
 
-  let n;
-  for (n = timeframe.unitOfTimeIndex - 1; n >= 0; n--) {
-    if (stemToNumber(stems[n]) === undefined) break;
+  // For the last...
+  // Set start to now - (duration * unitOfTime)
+  if (timeframe.raw.stems[n] === 'last') {
+    timeframe.raw.durationStartIndex = n;
+    timeframe.raw.durationStems.push(timeframe.raw.stems[n]);
 
-    timeframe.duration += stemToNumber(stems[n]);
-    timeframe.durationStems.push(stems[n]);
+    timeframe.duration = duration;
+    timeframe.start = moment.utc()
+      .startOf(timeframe.unitOfTime)
+      .subtract(duration, timeframe.unitOfTime)
+      .toISOString();
+    return;
   }
 
-  if (stems[n] === 'last') {
-    // set start to now - (duration * unitOfTime)
-  }
+  // Time in the future
+  // Set start to now
+  if (timeframe.raw.stems[n] === 'in') {
+    timeframe.raw.durationStartIndex = n;
+    timeframe.raw.durationStems.push(timeframe.raw.stems[n]);
 
-  timeframe.durationStartIndex = n + 1;
+    timeframe.duration = duration;
+    timeframe.start = moment.utc().startOf(timeframe.unitOfTime);
+    // return; // linter doesn't like unecessary returns
+  }
 }
 
+/*
+Returns a timeframe object
+{
+  unitOfTime : second/minute/hour/day/week/month/year
+  duration   : integer - timeframe lasts for <duration> * <unitOfTime>
+  start      : ISO datetime string
+}
+*/
 export default (classifiedInput) => {
-  const unitOfTimeIndex = findUnitOfTime(classifiedInput.ownerStems);
-  const unitOfTime = classifiedInput.ownerStems[unitOfTimeIndex];
-
   const timeframe = {
-    unitOfTime,
-    unitOfTimeIndex,
+    duration   : 1,
+    raw        : {
+      durationStems   : [],
+      unitOfTimeIndex : null,
+      stems           : classifiedInput.ownerStems,
+    },
+    start      : moment.utc().toISOString(),
+    unitOfTime : 'day',
   };
 
-  timeframe.duration = 1;
-  if (unitOfTimeIndex > 0) {
-    getDurationFromStems(classifiedInput.ownerStems, timeframe);
-  }
+  timeframe.raw.unitOfTimeIndex = findUnitOfTime(classifiedInput.ownerStems);
+  timeframe.raw.unitOfTimeStem  = classifiedInput.ownerStems[timeframe.raw.unitOfTimeIndex];
+  timeframe.unitOfTime          = unitOfTimeStems[timeframe.raw.unitOfTimeStem];
+
+  if (timeframe.raw.unitOfTimeIndex > 0) getDurationFromStems(timeframe);
 
   return timeframe;
 };
